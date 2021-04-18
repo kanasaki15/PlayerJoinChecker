6a77fa1f960032ff6907400480ae83299ad36f32
kanasaki15 <kassi_0123@kassi-hp.info>
2021-04-18 21:05:20 +0900

    準備

--- src/main/java/xyz/n7mn/dev/playerjoinchecker/CheckerListener.java
+++ src/main/java/xyz/n7mn/dev/playerjoinchecker/CheckerListener.java
@@ -5,14 +5,21 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.Plugin;
 
+import java.sql.*;
 import java.util.Collection;
 
 public class CheckerListener implements Listener {
+
+    private final Plugin plugin;
+    public CheckerListener(Plugin plugin){
+        this.plugin = plugin;
+    }
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void PlayerJoinEvent(PlayerJoinEvent e){
@@ -29,7 +36,7 @@ public class CheckerListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void PlayerQuitEvent (PlayerQuitEvent e){
+    public void PlayerQuitEvent(PlayerQuitEvent e){
         String name = e.getPlayer().getName();
         Collection<? extends Player> players = Bukkit.getServer().getOnlinePlayers();
 
@@ -40,6 +47,34 @@ public class CheckerListener implements Listener {
                 }
             }
         }).start();
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void AsyncPlayerPreLoginEvent (AsyncPlayerPreLoginEvent e){
+
+        try {
+            Connection con = DriverManager.getConnection("" +
+                    "jdbc:mysql://"+
+                    plugin.getConfig().getString("MySQLServer")+ ":"+plugin.getConfig().getInt("MySQLPort")+ "/"+
+                    plugin.getConfig().getString("MySQLDatabase")+
+                    plugin.getConfig().getString("MysqlOption"),
+                    plugin.getConfig().getString("MysqlUsername"),
+                    plugin.getConfig().getString("MysqlPassword")
+            );
+            con.setAutoCommit(true);
+
+            PreparedStatement statement = con.prepareStatement("SELECT * FROM MinecraftUserList WHERE MinecraftUUID = ?");
+            statement.setString(1, e.getUniqueId().toString());
+            ResultSet set = statement.executeQuery();
+
+
+            set.close();
+            statement.close();
+            con.close();
+        } catch (SQLException ex){
+            ex.printStackTrace();
+        }
+
     }
 
 }
--- src/main/java/xyz/n7mn/dev/playerjoinchecker/PlayerJoinChecker.java
+++ src/main/java/xyz/n7mn/dev/playerjoinchecker/PlayerJoinChecker.java
@@ -1,15 +1,43 @@
 package xyz.n7mn.dev.playerjoinchecker;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
 
 public final class PlayerJoinChecker extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // Plugin startup logic]
+        saveDefaultConfig();
 
-        getServer().getPluginManager().registerEvents(new CheckerListener(), this);
+        try {
+            boolean found = false;
+            Enumeration<Driver> drivers = DriverManager.getDrivers();
 
+            while (drivers.hasMoreElements()){
+                Driver driver = DriverManager.getDrivers().nextElement();
+                if (driver.equals(new com.mysql.cj.jdbc.Driver())){
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found){
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            }
+
+        } catch (SQLException e){
+            e.printStackTrace();
+            Bukkit.getServer().getPluginManager().disablePlugin(this);
+        }
+
+        getServer().getPluginManager().registerEvents(new CheckerListener(this), this);
+        getLogger().info(this.getName() + " " + this.getDescription().getVersion() + " Loaded!!");
     }
 
     @Override
