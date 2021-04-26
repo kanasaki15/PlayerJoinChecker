ad0c8eebf15162464ef7ff9b6432ed944b7235f5
kanasaki15 <kassi_0123@kassi-hp.info>
2021-04-27 03:41:24 +0900

    「/join」だけで今の入室権限がわからないと不便だなって思って

--- src/main/java/xyz/n7mn/dev/playerjoinchecker/JoinCommand.java
+++ src/main/java/xyz/n7mn/dev/playerjoinchecker/JoinCommand.java
@@ -20,7 +20,7 @@ public class JoinCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length != 1){
+        if (args.length != 1 && args.length != 0){
             return true;
         }
 
@@ -29,6 +29,63 @@ public class JoinCommand implements CommandExecutor {
             if (!player.isOp() && !player.hasPermission("7misys.op")){
                 return true;
             }
+        }
+
+        if (args.length == 0){
+
+            int nowRank = plugin.getConfig().getInt("DefaultJoinPermRank");
+
+            new Thread(()->{
+                try {
+                    boolean found = false;
+                    Enumeration<Driver> drivers = DriverManager.getDrivers();
+
+                    while (drivers.hasMoreElements()){
+                        Driver driver = drivers.nextElement();
+                        if (driver.equals(new com.mysql.cj.jdbc.Driver())){
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found){
+                        DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+                    }
+
+                    Connection con = DriverManager.getConnection("" +
+                                    "jdbc:mysql://"+
+                                    plugin.getConfig().getString("MySQLServer")+ ":"+plugin.getConfig().getInt("MySQLPort")+ "/"+
+                                    plugin.getConfig().getString("MySQLDatabase")+
+                                    plugin.getConfig().getString("MySQLOption"),
+                            plugin.getConfig().getString("MySQLUsername"),
+                            plugin.getConfig().getString("MySQLPassword")
+                    );
+                    con.setAutoCommit(true);
+
+                    PreparedStatement statement = con.prepareStatement("SELECT * FROM RoleRankList");
+                    ResultSet set = statement.executeQuery();
+
+                    while (set.next()){
+
+                        if (set.getInt("Rank") != nowRank){
+                            continue;
+                        }
+
+                        sender.sendMessage(ChatColor.YELLOW + "[ななみ鯖] " + ChatColor.RESET + "現在の入室権限 " + set.getString("Name") + " 以上\n設定するには /join <権限レベル> を実行してください。");
+                        break;
+
+                    }
+
+                    set.close();
+                    statement.close();
+
+                    con.close();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }).start();
+
+            return true;
         }
 
         try {
